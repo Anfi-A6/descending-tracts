d606628f0d80896ceb8e30a08a782fccd0f5709e
AramLR <aramluck2007@gmail.com>
2026-05-01 20:04:10 -0600

    feat: add pwa and offline support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,29 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
-
-import svelte from "@astrojs/svelte";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [
+    AstroPWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Modelo de Neuroanatomía Mesa 6: Tractos de salida de puente y mesencéfalo",
+        short_name: "Neuro Mesa 6",
+        description:
+          "Página web para representar de manera virtual el modelo de tractos de salida de puente y mesencéfalo",
+        theme_color: "#0049af",
+        background_color: "#0049af",
+        display: "standalone",
+        start_url: "/",
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2,jpg,webp}"],
+      },
+    }),
+  ],
 });
