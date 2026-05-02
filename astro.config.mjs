33d2f65da9d57474e8e8e32b3e65afc5c4a2e224
AramLR <aramluck2007@gmail.com>
2026-05-01 19:23:33 -0600

    fix: removed useless svelte integration and removed not working pwa service

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,33 +2,12 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
-import { VitePWA } from "vite-plugin-pwa";
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [
-      tailwindcss(),
-      VitePWA({
-        registerType: "autoUpdate",
-        manifest: {
-          name: "Modelo de Neuroanatomía Mesa 6: Tractos de salida de puente y mesencéfalo",
-          short_name: "Neuro Mesa 6",
-          description:
-            "Página web para representar de manera virtual el modelo de tractos de salida de puente y mesencéfalo",
-          theme_color: "#0049af",
-          background_color: "#0049af",
-          display: "standalone",
-          start_url: "/",
-        },
-        workbox: {
-          globPatterns: ["**/*.(js,css,html,ico,png,svg,woff2)"],
-        },
-      }),
-    ],
+    plugins: [tailwindcss()],
   },
-
-  integrations: [svelte()],
 });
